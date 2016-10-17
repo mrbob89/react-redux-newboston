7b3397590208b8da5d6295a84d588855e7cc6f1e
Roman Boboshko <romanboboshko89@gmail.com>
2016-10-18 01:27:43 +0300

    (Reducers) - Add all reducers to store

---
+++ dev/js/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import UserReducer from './reducerUsers';
+
+const allReducers = combineReducers({
+    users: UserReducer
+});
+
+export default allReducers;
