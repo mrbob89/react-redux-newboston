61c631afe553ff8faefdb57bd191c81e943f7540
Roman Boboshko <romanboboshko89@gmail.com>
2016-10-18 02:20:00 +0300

    Listening for actions

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import UserReducer from './reducerUsers';
+import ActiveUserReducer from './reducerActiveUser';
 
 const allReducers = combineReducers({
-    users: UserReducer
+    users: UserReducer,
+    activeUser: ActiveUserReducer
 });
 
 export default allReducers;
